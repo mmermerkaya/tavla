b376a66e60b8d259cb6d113e4e2191896b38cdff
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2015-08-23 12:54:16 +0300

    Improved cell css classes and click events.

--- client/main.js
+++ client/main.js
@@ -21,39 +21,31 @@ Template.board.helpers({
 });
 
 Template.cell.helpers({
-  selectedClass: function() {
+  cssClass: function() {
+    var board = Boards.findOne();
+
     if (this.id == Session.get('selected')) {
       return 'selected';
     }
-  },
-
-  availableClass: function() {
-    if (Session.get('selected') !== null) {
-      var board = Boards.findOne();
-      return board.dice.some(function(die) {
-        if (this.id == Session.get('selected') + die) {
-          return true;
-        }
-        return false;
-      }, this) ? 'available' : 'unavailable';
+    if (Session.get('selected') !== null && board.dice.indexOf(this.id - Session.get('selected')) !== -1) {
+      return 'moveable';
     }
-    return 'unavailable';
+    return 'idle';
   }
 })
 
 Template.cell.events({
-  'click .unavailable': function clicked (event) {
-    if (Session.get('selected') === this.id) {
-      console.log('deselected ' + this.id);
-      Session.set('selected', null);
-    }
-    else {
-      console.log('selected ' + this.id);
-      Session.set('selected', this.id);
-    }
+  'click .idle': function idle (event) {
+    console.log('selected ' + this.id);
+    Session.set('selected', this.id);
   },
 
-  'click .available': function movePiece (event) {
+  'click .selected': function selected (event) {
+    console.log('deselected ' + this.id);
+    Session.set('selected', null);
+  },
+
+  'click .moveable': function available (event) {
     console.log('moving ' + Session.get('selected') + ' to ' + this.id);
     Meteor.call('movePiece', Session.get('selected'), this.id, function(error, response){});
     Session.set('selected', null);
