ac3389c2f7c9c04dd0c5d8f4f28e48bdb1a81a5a
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2015-08-23 11:13:49 +0300

    Moved game data into collection and fixed indentation in some places.

--- client/main.js
+++ client/main.js
@@ -1,114 +1,23 @@
-var piecesData = [
-  {
-    color: 1,
-    count: 2
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: 0,
-    count: 5
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: 0,
-    count: 3
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: 1,
-    count: 5
-  },
-  {
-    color: 0,
-    count: 5
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: 1,
-    count: 3
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: 1,
-    count: 5
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: -1,
-    count: 0
-  },
-  {
-    color: 0,
-    count: 2
-  },
-];
-
-piecesData.forEach(function(data, i) {
-  data.id = i;
-}),
-
 Meteor.startup(function () {
   Session.set('selected', null);
 });
 
 Template.board.helpers({
-  topRow: piecesData.slice(12, 24),
+  topRow: function() {
+    var board = Boards.findOne();
+    if(!board) {
+      return null;
+    }
+    return board.data.slice(12, 24);
+  },
 
-  bottomRow: piecesData.slice(0, 12).reverse()
+  bottomRow: function() {
+    var board = Boards.findOne();
+    if(!board) {
+      return null;
+    }
+    return board.data.slice(0, 12).reverse();
+  }
 });
 
 Template.cell.helpers({
--- server/main.js
+++ server/main.js
@@ -1,5 +1,117 @@
 Meteor.methods({
-    movePiece: function(from, to) {
-        console.log(Meteor.userId() + ' requested moving from ' + from + ' to ' + to);
-    }
+  movePiece: function(from, to) {
+    console.log(Meteor.userId() + ' requested moving from ' + from + ' to ' + to);
+  }
+});
+
+Meteor.startup(function () {
+  if (Boards.find().count() === 0) {
+    var piecesData = [
+    {
+      color: 1,
+      count: 2
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: 0,
+      count: 5
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: 0,
+      count: 3
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: 1,
+      count: 5
+    },
+    {
+      color: 0,
+      count: 5
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: 1,
+      count: 3
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: 1,
+      count: 5
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: -1,
+      count: 0
+    },
+    {
+      color: 0,
+      count: 2
+    },
+    ];
+
+    piecesData.forEach(function(data, i) {
+      data.id = i;
+    }),
+
+    Boards.insert({
+      data: piecesData
+      dice: [6, 2]
+    });
+  }
 });
