e2ff7aee8eddf2b0c1c0ef328056a89b38884da2
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2015-09-14 23:03:09 +0300

    Reimplemented moving a piece.

--- client/components/board.jsx
+++ client/components/board.jsx
@@ -10,31 +10,65 @@ Board = React.createClass({
     },
 
     topRow: function() {
-      return this.data.game.board.slice(12, 24);
+        return this.data.game.board.slice(12, 24);
     },
 
     bottomRow: function() {
-      return this.data.game.board.slice(0, 12).reverse();
+        return this.data.game.board.slice(0, 12).reverse();
+    },
+
+    cssClass(cellId) {
+        if (cellId == Session.get('selected')) {
+            return 'selected';
+        }
+
+        if (Session.get('selected') !== null) {
+            var dir = this.data.game.turn % 2 ? 1: -1;
+            if (this.data.game.dice.indexOf((cellId * -dir) + (Session.get('selected') * dir)) !== -1) {
+                return 'moveable';
+            }
+        }
+
+        return 'idle';
+    },
+
+    cellClickHandler(cellId) {
+        console.log(cellId);
+        if (this.cssClass(cellId) === 'idle') {
+            console.log('selected ' + cellId);
+            Session.set('selected', cellId);
+        }
+        else if (this.cssClass(cellId) === 'selected') {
+            console.log('deselected ' + cellId);
+            Session.set('selected', null);
+        }
+        else if (this.cssClass(cellId) === 'moveable') {
+            console.log('moving ' + Session.get('selected') + ' to ' + cellId);
+            Meteor.call('movePiece', FlowRouter.getParam('gameId'), Session.get('selected'), cellId);
+            Session.set('selected', null);
+        }
+        this.forceUpdate();
+    },
+
+    renderCell(cell) {
+        var cssClass = this.cssClass(cell.id);
+        return (<Cell cellData={cell} cssClass={cssClass} key={cell.id} clickHandler={this.cellClickHandler} />);
     },
 
     render() {
         var topRow = this.topRow();
         var bottomRow = this.bottomRow();
-        console.log(topRow);
+
         return (
             <div className="board">
-              <div className="row top">
-                  {topRow.map(function(cell) {
-                      return (<Cell cellData={cell} key={cell.id} />);
-                  })}
-              </div>
-              <div className="row bottom">
-                  {bottomRow.map(function(cell) {
-                      return (<Cell cellData={cell} key={cell.id} />);
-                  })}
-              </div>
-              {this.data.game.dice}
-              {this.data.game.turn}
+                <div className="row top">
+                    {topRow.map(this.renderCell)}
+                </div>
+                <div className="row bottom">
+                    {bottomRow.map(this.renderCell)}
+                </div>
+                {this.data.game.dice}
+                {this.data.game.turn}
             </div>
         );
     }
--- client/components/cell.jsx
+++ client/components/cell.jsx
@@ -1,39 +1,13 @@
 Cell = React.createClass({
-/*
-    cssClass() {
-        if (this.data.id == Session.get('selected')) {
-            return 'selected';
-        }
-
-        if (Session.get('selected') !== null) {
-            var board = Games.findOne({_id: FlowRouter.getParam('gameId')});
-            var dir = board.turn % 2 ? 1: -1;
-            if (board.dice.indexOf((this.id * -dir) + (Session.get('selected') * dir)) !== -1) {
-                return 'moveable';
-            }
-        }
-        return 'idle';
+    clickHandler(event) {
+        this.props.clickHandler(this.props.cellData.id);
     },
 
-    'click .idle': function idle(event) {
-      console.log('selected ' + this.id);
-      Session.set('selected', this.id);
-    },
-
-    'click .selected': function selected(event) {
-      console.log('deselected ' + this.id);
-      Session.set('selected', null);
-    },
-
-    'click .moveable': function available(event) {
-      console.log('moving ' + Session.get('selected') + ' to ' + this.id);
-      Meteor.call('movePiece', FlowRouter.getParam('gameId'), Session.get('selected'), this.id);
-      Session.set('selected', null);
-    }
-*/
     render() {
+        var className = "cell " + this.props.cssClass;
+
         return (
-            <div className="cell">
+            <div className={className} onClick={this.clickHandler}>
                 {this.props.cellData.id}
                 <br/>
                 {this.props.cellData.color} {this.props.cellData.count}
