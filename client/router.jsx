74db747e34544197fb2dc5cda5566aa0db745d38
Mertcan Mermerkaya <mertcan@mermerkaya.net>
2016-01-16 14:55:29 +0200

    Added BootstrapXL large screen fix. Changed Bootstrap package and removed Bootswatch cdn.

--- client/router.jsx
+++ client/router.jsx
@@ -1,7 +1,7 @@
 Reaktor.init(
     <Router>
-      <Route path="/" layout={App} content={LandingPage} name="Home" />
-      <Route path="/about" layout={App} content={About} name="About" />
-      <Route path="/game/:gameId" layout={App} content={Game} name="Game" />
+        <Route path="/" layout={App} content={LandingPage} name="Home" />
+        <Route path="/about" layout={App} content={About} name="About" />
+        <Route path="/game/:gameId" layout={App} content={Game} name="Game" />
     </Router>
 );
